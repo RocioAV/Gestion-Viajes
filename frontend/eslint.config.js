4f703adee3977a83892462124b7605cccb5d81b0
RocioAV <rocio.velazquez.0212@gmail.com>
2026-06-17 19:37:27 -0300

    Inicializacion proyecto frontend con React-Typescript

---
+++ frontend/eslint.config.js
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+import { types } from 'node:util'
+
+export default antfu({
+  react: true,
+  typescript: true,
+  stylistic: {
+    indent: 2, // 4, or 'tab'
+    quotes: 'single', // or 'double'
+    braceStyle: 'stroustrup', // '1tbs', or 'allman'
+  },
+
+  jsonc: false,
+  yaml: false,
+})
