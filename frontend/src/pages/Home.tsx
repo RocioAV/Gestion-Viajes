041e91e37af2059fb2fdf5ba8e17a94619281fe9
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-06-19 19:06:09 -0300

    react router dom y las rutas de registro de operadores y login

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-primary text-3xl font-bold">Home</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/admin/Registro.tsx
@@ -0,0 +1,9 @@
+function Registro() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-primary text-3xl font-bold">Registro de Operadores</h1>
+    </div>
+  )
+}
+
+export default Registro
