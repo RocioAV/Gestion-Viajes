ed54fdf23ba29978c4672e72583322bc1b7075bf
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-07-16 00:58:03 -0300

    arreglo en la conexion de prisma

--- backend/src/lib/prisma.ts
+++ backend/src/lib/prisma.ts
@@ -2,7 +2,7 @@ import process from 'node:process'
 import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '../../generated/prisma/client'
 
-const connectionString = process.env.DATABASE_URL!.split('?')[0]
+const connectionString = process.env.DATABASE_URL!
 
 const adapter = new PrismaPg({
   connectionString,
