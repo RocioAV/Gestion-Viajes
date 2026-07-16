9301d97b2dcbb13fb546d9f27c1cef23ce91e8f5
Mauricio Velazquez <mauri_i9070@hotmail.com>
2026-07-16 17:49:55 -0300

    fix: se agrego la variable de entorno de la base url

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,6 +1,6 @@
 import { getToken, removeToken, removeUser } from './auth'
 
-const BASE_URL = 'http://localhost:4000/api'
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export async function apiClient<T>(
   path: string,
